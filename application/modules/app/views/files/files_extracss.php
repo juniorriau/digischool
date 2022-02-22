d7249efe8940ff551867931cffb53b71bbe22792
juniorriau <juniorriau18@gmail.com>
2022-02-23 03:00:17 +0700

    update app : add extrajs extracss files

---
+++ application/modules/app/views/files/files_extracss.php
@@ -0,0 +1,1 @@
+<link href="<?php echo base_url() ?>assets/app/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
---
+++ application/modules/app/views/files/files_extrajs.php
@@ -0,0 +1,7 @@
+<script src="<?php echo base_url() ?>assets/app/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript">
+    jQuery('#expiredat').datepicker({
+            format: "yyyy-mm-dd",
+            clearBtn: true,
+        });
+</script>
