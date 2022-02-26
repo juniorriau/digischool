739e18fed0c8b5a0fdb78d7c0d634992e8e2b90d
juniorriau <juniorriau18@gmail.com>
2022-02-26 23:07:09 +0700

    update web : add post list view

---
+++ application/modules/web/views/posts.php
@@ -0,0 +1,209 @@
+
+				<div class="posts_sidebar clearfix"><!--Start Posts Areaa -->
+					<div class="posts_areaa col-md-8"><!-- posts_areaa -->
+						<div class="row">
+							
+							<!-- block_posts block_2 -->
+							<div class="block_posts block_2">
+								<div class="featured_title"><!-- featured_title -->
+									<h4>Kegiatan Sekolah</h4>
+									<a href="#" class="view_button">View All</a>
+								</div><!-- // featured_title -->
+								<!-- block_inner -->
+								<div class="block_inner row">
+									<!-- small_list_post -->
+									<div class="small_list_post col-lg-6 col-md-6 col-sm-6 col-xs-6">
+										<ul>
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/2.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Elit Sed Do Eiusmod Tempor Incididunt</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/8.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>incididunt ut dolore magna</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/4.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Consectetur Adipisicing Elit, Sed Do Eiusmod</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+										</ul>
+									</div>
+									<!-- // small_list_post -->
+
+									<!-- small_list_post -->
+									<div class="small_list_post col-lg-6 col-md-6 col-sm-6 col-xs-6">
+										<ul>
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/5.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5> velit esse cillum dolore eu fugiat nulla pariatur</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/6.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Elit Sed Do Eiusmod Tempor Incididunt</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/7.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>incididunt ut dolore magna</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											
+
+										</ul>
+									</div>
+									<!-- // small_list_post -->
+								</div>
+								<!-- // block_inner -->
+							</div>
+							<!-- // block_posts block_2 -->
+
+                            <!-- block_posts block_2 -->
+							<div class="block_posts block_2">
+								<div class="featured_title"><!-- featured_title -->
+									<h4>Kegiatan Siswa</h4>
+									<a href="#" class="view_button">View All</a>
+								</div><!-- // featured_title -->
+								<!-- block_inner -->
+								<div class="block_inner row">
+									<!-- small_list_post -->
+									<div class="small_list_post col-lg-6 col-md-6 col-sm-6 col-xs-6">
+										<ul>
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/2.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Elit Sed Do Eiusmod Tempor Incididunt</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/8.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>incididunt ut dolore magna</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/4.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Consectetur Adipisicing Elit, Sed Do Eiusmod</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+										</ul>
+									</div>
+									<!-- // small_list_post -->
+
+									<!-- small_list_post -->
+									<div class="small_list_post col-lg-6 col-md-6 col-sm-6 col-xs-6">
+										<ul>
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/5.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5> velit esse cillum dolore eu fugiat nulla pariatur</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/6.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>Elit Sed Do Eiusmod Tempor Incididunt</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											<li class="small_post clearfix">
+												<div class="img_small_post">
+													<img src="img/news/sport/7.jpg" alt="Consectetur adipisicing elit">
+												</div>
+												<div class="small_post_content">
+													<div class="title_small_post">
+														<a href="#"><h5>incididunt ut dolore magna</h5></a>
+													</div>
+													<div class="post_date"><em><a href="#">July 01, 2014</a></em></div>
+												</div>
+											</li>
+
+											
+
+										</ul>
+									</div>
+									<!-- // small_list_post -->
+								</div>
+								<!-- // block_inner -->
+							</div>
+							<!-- // block_posts block_2 -->
+						</div>
+					</div><!--End Posts Areaa -->
+					<?php $this->load->view('base/sidebar'); ?>
+					
+				</div><!-- Posts And Sidebar -->
