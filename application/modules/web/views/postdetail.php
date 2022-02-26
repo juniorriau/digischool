966a5890d8a64371ff130441d72bccb40be74818
juniorriau <juniorriau18@gmail.com>
2022-02-26 23:07:26 +0700

    update web : add postdetail

---
+++ application/modules/web/views/postdetail.php
@@ -0,0 +1,14 @@
+				<div class="posts_sidebar clearfix"><!--Start Posts Areaa -->
+					<div class="posts_areaa col-md-8"><!-- posts_areaa -->
+						<div class="row">
+							
+							<!-- block_posts block_2 -->
+							<div class="block_posts block_2">
+
+							</div>
+
+						</div>
+					</div><!--End Posts Areaa -->
+					<?php $this->load->view('base/sidebar'); ?>
+					
+				</div><!-- Posts And Sidebar -->
