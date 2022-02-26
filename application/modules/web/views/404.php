b36297a43b5ff8bd5eadd8b3dfa9239d1f711ca8
juniorriau <juniorriau18@gmail.com>
2022-02-26 22:59:41 +0700

    update web : modify 404 template

--- application/modules/web/views/404.php
+++ application/modules/web/views/404.php
@@ -1,44 +1,10 @@
-<section class="page-header page-header-xs">
-    <div class="container">
-
-        <h1><?php echo strtoupper($this->uri->segment(2)) ?></h1>
-
-        <!-- breadcrumbs -->
-        <ol class="breadcrumb">
-            <li><a href="<?php echo base_url() ?>">Home</a></li>
-            <li><a href="<?php echo base_url($this->uri->segment(1)) ?>"><?php echo ucfirst($this->uri->segment(1)) ?></a></li>
-            <li class="active"><?php echo ucfirst($this->uri->segment(2)) ?></li>
-        </ol><!-- /breadcrumbs -->
-
-    </div>
-</section>
-<section class="section-xlg">
-    <div class="container">
-
-        <div class="row">
-
-            <div class="col-md-6 col-sm-6 hidden-xs-down">
-
-                <div class="error-404">
-                    404
+<div class="col-md-4 col-md-offset-4 text-center">
+                    <div class="not-found-content">
+                        <h1>404</h1>
+                        <h3>Opsss! We are sorry</h3>
+                        <p>The page your are looking for cannot be found. Try using our search box.</p>
+                    </div>                                   
+                    <div class="back-to-home">
+                        <a href="<?php echo base_url()?>">Go to Homepage</a>
+                    </div>
                 </div>
-
-            </div>
-
-            <div class="col-md-6 col-sm-6">
-
-                <h3 class="m-0">Sorry, <strong>The page/post you requested can not be found!</strong></h3>
-                <p class="mt-0 fs-20 font-lato text-muted">Please, try again.</p>
-
-                <!-- /INLINE SEARCH -->
-
-                <div class="divider mb-0"><!-- divider --></div>
-
-                <a class="fs-16 font-lato" href="<?php echo base_url() ?>"><i class="glyphicon glyphicon-menu-left mr-10 fs-12"></i> back to homepage now!</a>
-
-            </div>
-
-        </div>
-
-    </div>
-</section>
