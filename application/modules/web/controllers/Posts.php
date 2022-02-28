195b6fabb87619bb178f4983f1eeb766f5ebdd14
juniorriau <juniorriau18@gmail.com>
2022-02-28 10:49:40 +0700

    update web : modify posts controller

--- application/modules/web/controllers/Posts.php
+++ application/modules/web/controllers/Posts.php
@@ -1,53 +1,46 @@
 <?php
 
-defined('BASEPATH') or exit('No direct script access allowed');
+defined("BASEPATH") or exit("No direct script access allowed");
 
-class Posts extends MY_Controller
-{
+class Posts extends MY_Controller {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
-        $this->load->model('app/Posts_model');
-        $this->load->model('app/Categories_model');
-        $this->load->model('app/Comments_model');
+        $this->load->model("app/Posts_model");
+        $this->load->model("app/Categories_model");
+        $this->load->model("app/Comments_model");
     }
 
-    public function category($category)
-    {
-
-    }
-    public function read($slug)
-    {
-        $post = $this->Posts_model->get_by_slug($slug, 'Public');
+    public function read($slug) {
+        $post = $this->Posts_model->get_by_slug($slug, "Public");
         $comments = $this->Comments_model->get_by_postid($post->id);
         $category = $this->Categories_model->get_all();
+        $recentpost = $this->Posts_model->get_public_post($this->config->item("site_limit_post"));
+        $categorycount = $this->Categories_model->get_posts_count();
         if (!empty($post)) {
             $data = array(
-                "template" => 'postdetail',
+                "template" => "postdetail",
                 "post" => $post,
                 "comments" => $comments,
                 "category" => $category,
+                "recentpost" => $recentpost,
+                "categorycount" => $categorycount,
             );
-        }
-        else {
+        } else {
             $data = array(
-                "template" => '404',
+                "template" => "404",
                 "post" => $post,
                 "comments" => $comments,
                 "category" => $category,
+                "recentpost" => $recentpost,
+                "categorycount" => $categorycount,
             );
         }
-        $this->load->view('base/content', $data);
+        $this->load->view("base/content", $data);
     }
 
-    public function search($keyword)
-    {
+    public function comment_action() {
 
     }
 
-    public function comment_action()
-    {
-
-    }
 }
