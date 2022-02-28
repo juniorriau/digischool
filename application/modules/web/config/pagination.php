4447400fd8026ddd228a186d42d21be1f1b7b8f0
juniorriau <juniorriau18@gmail.com>
2022-02-28 10:49:02 +0700

    update web : modify paginatio config

--- application/modules/web/config/pagination.php
+++ application/modules/web/config/pagination.php
@@ -1,14 +1,14 @@
 <?php
 
 $config['query_string_segment'] = 'start';
-
+$config['display_pages'] = FALSE;
 $config['full_tag_open'] = '<nav class="pagination_post"><ul class="pager">';
 $config['full_tag_close'] = '</ul></nav>';
 
-$config['prev_link'] = 'Sebelumnya';
+$config['prev_link'] = 'Prev';
 $config['prev_tag_open'] = '<li>';
 $config['prev_tag_close'] = '</li>';
 
-$config['next_link'] = 'Selanjutnya';
+$config['next_link'] = 'Next';
 $config['next_tag_open'] = '<li>';
 $config['next_tag_close'] = '</li>';
