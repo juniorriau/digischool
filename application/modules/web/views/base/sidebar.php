3588f566e610269f6888eff7785f04300846f8cb
juniorriau <juniorriau18@gmail.com>
2022-02-28 10:50:05 +0700

    update web : modify sidebar web add category when openning post list

--- application/modules/web/views/base/sidebar.php
+++ application/modules/web/views/base/sidebar.php
@@ -1,56 +1,77 @@
 <div class="sidebar col-md-4"><!--Start Sidebar -->
-						<div class="row">
-							<div class="inner_sidebar"><!--Start Inner Sidebar -->
+    <div class="row">
+        <div class="inner_sidebar"><!--Start Inner Sidebar -->
 
-								<div class="widget widget_social_counter"> <!-- widget_social_counter -->
-									<h4 class="widget_title">Follow Us</h4>
-									<div class="social_counter social_counter_twitter">
-										<a class="social_counter_icon" href="<?php echo $this->config->item('social_twitter')?>" title="Follow our twitter" target="_blank"><i class="fa fa-twitter"></i></a>
-										
-                                        <div class="clearfix"></div>
-									</div>
-									<div class="social_counter social_counter_facebook">
-										<a class="social_counter_icon" href="<?php echo $this->config->item('social_facebook')?>" title="Like our facebook" target="_blank"><i class="fa fa-facebook"></i></a>
-										
-										<div class="clearfix"></div>
-									</div>
-									<div class="social_counter social_counter_instagram">
-										<a class="social_counter_icon" href="<?php echo $this->config->item('social_instagram')?>" title="Follow our instagram" target="_blank"><i class="fa fa-instagram"></i></a>
-										
-										<div class="clearfix"></div>
-									</div>
-									<div class="social_counter social_counter_youtube">
-										<a class="social_counter_icon" href="<?php echo $this->config->item('social_youtube')?>" title="Subscribe our youtube" target="_blank"><i class="fa fa-youtube"></i></a>
+            <div class="widget widget_social_counter"> <!-- widget_social_counter -->
+                <h4 class="widget_title">Follow Us</h4>
+                <div class="social_counter social_counter_twitter">
+                    <a class="social_counter_icon" href="<?php echo $this->config->item('social_twitter') ?>" title="Follow our twitter" target="_blank"><i class="fa fa-twitter"></i></a>
 
-										<div class="clearfix"></div>
-									</div>
-									
-									<div class="clearfix"></div>
-								</div><!-- End widget_social_counter -->
-									
+                    <div class="clearfix"></div>
+                </div>
+                <div class="social_counter social_counter_facebook">
+                    <a class="social_counter_icon" href="<?php echo $this->config->item('social_facebook') ?>" title="Like our facebook" target="_blank"><i class="fa fa-facebook"></i></a>
 
-								<div class="widget widget_recent_post"><!-- Start widget recent post -->
-									<h4 class="widget_title">Recent Post</h4>
-									<ul class="recent_post">
-										<?php
-										if(!empty($recentpost)){
-											foreach($recentpost as $recent){  ?>
-										<li>
-											<figure class="widget_post_thumbnail">
-												<a href="#"><img src="<?php echo base_url($recent->postimagethumb)?>" alt="<?php echo $recent->title?>"></a>
-											</figure>
-											<div class="widget_post_info">
-												<h5><a href="<?php echo base_url("web/posts/read/".$recent->slug)?>"><?php echo $recent->title?></a></h5>
-												<div class="post_meta">
-													<span class="date_meta"><a href="#"><i class="fa fa-calendar"></i><?php echo date_format(date_create($recent->createdat), "d, M Y")?></a></span>
-												</div>
-											</div>
-										</li>
-											<?php }
-										} ?>
-									</ul>
-								</div><!-- End widget recent post -->
-								
-							</div><!--End Inner Sidebar -->
-						</div>
-					</div><!--End Sidebar -->
+                    <div class="clearfix"></div>
+                </div>
+                <div class="social_counter social_counter_instagram">
+                    <a class="social_counter_icon" href="<?php echo $this->config->item('social_instagram') ?>" title="Follow our instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+
+                    <div class="clearfix"></div>
+                </div>
+                <div class="social_counter social_counter_youtube">
+                    <a class="social_counter_icon" href="<?php echo $this->config->item('social_youtube') ?>" title="Subscribe our youtube" target="_blank"><i class="fa fa-youtube"></i></a>
+
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div><!-- End widget_social_counter -->
+
+
+            <div class="widget widget_recent_post"><!-- Start widget recent post -->
+                <h4 class="widget_title">Recent Post</h4>
+                <ul class="recent_post">
+                    <?php
+                    if (!empty($recentpost)) {
+                        foreach ($recentpost as $recent) {
+                            ?>
+                            <li>
+                                <figure class="widget_post_thumbnail">
+                                    <a href="#"><img src="<?php echo base_url($recent->postimagethumb) ?>" alt="<?php echo $recent->title ?>"></a>
+                                </figure>
+                                <div class="widget_post_info">
+                                    <h5><a href="<?php echo base_url("web/posts/read/" . $recent->slug) ?>"><?php echo $recent->title ?></a></h5>
+                                    <div class="post_meta">
+                                        <span class="date_meta"><a href="#"><i class="fa fa-calendar"></i><?php echo date_format(date_create($recent->createdat), "d, M Y") ?></a></span>
+                                    </div>
+                                </div>
+                            </li>
+                            <?php
+                        }
+                    }
+                    ?>
+                </ul>
+            </div><!-- End widget recent post -->
+
+            <?php if ($this->uri->segment(2) == 'posts' || $this->uri->segment(2) == 'categories') { ?>
+                <div class="widget widget_categories"><!-- Start widget categories -->
+                    <h4 class="widget_title">Categories</h4>
+                    <ul class="categories">
+                        <?php
+                        if (!empty($categorycount)) {
+                            foreach ($categorycount as $cc) {
+                                ?>
+                                <li><a href="<?php echo base_url('web/categories/index/' . $cc->slug) ?>"><?php echo $cc->category ?> <span>(<?php echo $cc->postcount ?>)</span></a><i class="fa fa-angle-double-right"></i></li>
+                                    <?php
+                                }
+                            }
+                            ?>
+                    </ul>
+                </div><!--End widget categories-->
+            <?php }
+            ?>
+
+        </div><!--End Inner Sidebar -->
+    </div>
+</div><!--End Sidebar -->
