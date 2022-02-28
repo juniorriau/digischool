3dbc9b16ff578832dbc9384a27d91436be4c5190
juniorriau <juniorriau18@gmail.com>
2022-02-28 08:47:57 +0700

    update web : modify pagination config

--- application/modules/web/config/pagination.php
+++ application/modules/web/config/pagination.php
@@ -2,17 +2,13 @@
 
 $config['query_string_segment'] = 'start';
 
-$config['full_tag_open'] = '<div class="pagination fwmpag">';
-$config['full_tag_close'] = '</div>';
+$config['full_tag_open'] = '<nav class="pagination_post"><ul class="pager">';
+$config['full_tag_close'] = '</ul></nav>';
 
-$config['prev_link'] = '<i class="fas fa-caret-left"></i> <span>Prev</span>';
+$config['prev_link'] = 'Sebelumnya';
+$config['prev_tag_open'] = '<li>';
+$config['prev_tag_close'] = '</li>';
 
-$config['next_link'] = '<span>Next</span> <i class="fas fa-caret-right"></i>';
-
-$config['first_link'] = '<i class="fas fa-angle-double-left"></i> <span>Prev</span>';
-
-$config['last_link'] = '<span>Last</span> <i class="fas fa-angle-double-right"></i>';
-
-$config['cur_tag_open'] = '<a href="#" class="current-page">';
-$config['cur_tag_close'] = '</a>';
-
+$config['next_link'] = 'Selanjutnya';
+$config['next_tag_open'] = '<li>';
+$config['next_tag_close'] = '</li>';
