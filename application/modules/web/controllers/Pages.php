98b4b3cfae574faf238918c39b5c35c87072c0b6
juniorriau <juniorriau18@gmail.com>
2022-03-01 09:04:35 +0700

    update web : modify pages controller

--- application/modules/web/controllers/Pages.php
+++ application/modules/web/controllers/Pages.php
@@ -2,25 +2,29 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pages extends MY_Controller
-{
+class Pages extends MY_Controller {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
         $this->load->model('app/Pages_model');
+        $this->load->model("app/Posts_model");
+        $this->load->model("app/Categories_model");
     }
 
-    public function index()
-    {
+    public function index() {
         $page = $this->Pages_model->get_by_slug('page', $this->uri->segment(3));
+        $category = $this->Categories_model->get_all();
+        $recentpost = $this->Posts_model->get_public_post($this->config->item("site_limit_post"));
+        $categorycount = $this->Categories_model->get_posts_count();
         if (!empty($page)) {
             $data = array(
                 "template" => 'pages',
                 "page" => $page,
+                "category" => $category,
+                "recentpost" => $recentpost,
+                "categorycount" => $categorycount,
             );
-        }
-        else {
+        } else {
             $data = array(
                 "template" => '404',
             );
